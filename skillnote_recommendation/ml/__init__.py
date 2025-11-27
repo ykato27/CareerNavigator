e34ae2920a7281b07cc50a55f54d9fda26cb3c93
ykato <y.kato930@gmail.com>
2025-11-27 09:11:11 +0900

    fix: 循環インポートエラーを修正 (ml/__init__.py のインポート順序変更)

--- skillnote_recommendation/ml/__init__.py
+++ skillnote_recommendation/ml/__init__.py
@@ -4,10 +4,11 @@
 Matrix Factorization、多様性再ランキング、MLベース推薦エンジンを提供
 """
 
+# 循環インポートを避けるため、exceptionsを最初にインポート
+from skillnote_recommendation.ml.exceptions import ColdStartError, MLModelNotTrainedError
 from skillnote_recommendation.ml.matrix_factorization import MatrixFactorizationModel
 from skillnote_recommendation.ml.diversity import DiversityReranker
 from skillnote_recommendation.ml.ml_recommender import MLRecommender
-from skillnote_recommendation.ml.exceptions import ColdStartError, MLModelNotTrainedError
 from skillnote_recommendation.ml.causal_structure_learner import CausalStructureLearner
 from skillnote_recommendation.ml.causal_graph_recommender import CausalGraphRecommender
 from skillnote_recommendation.ml.unified_sem_estimator import UnifiedSEMEstimator
@@ -22,11 +23,11 @@ except ImportError:
     _HAS_NODE2VEC = False
 
 __all__ = [
+    "ColdStartError",
+    "MLModelNotTrainedError",
     "MatrixFactorizationModel",
     "DiversityReranker",
     "MLRecommender",
-    "ColdStartError",
-    "MLModelNotTrainedError",
     "CausalStructureLearner",
     "CausalGraphRecommender",
     "UnifiedSEMEstimator",
